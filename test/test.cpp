a73398cbc09eea90a9af9132cc448321432b591e
Lucas Heer <lucas.heer@online.de>
2023-06-10 16:01:02 +0200

    Add more API tests for setting data, add gprof support

--- test/test.cpp
+++ test/test.cpp
@@ -12,6 +12,8 @@ UTEST_STATE();
 
 using client = json_client::EndpointConnection;
 using basic_type = json_client::types::BasicType;
+using compound_type = json_client::types::CompoundType;
+
 
 //
 // Tests for reading data
@@ -42,7 +44,7 @@ UTEST(Get, basic_string)
 
 UTEST(Get, vector_homogenous)
 {
-    const auto vec = client("/array/homogenous").get<std::vector<basic_type>>();
+    const auto vec = client("/array/homogenous").get<compound_type>();
     ASSERT_EQ(vec.size(), 19);
 
     std::size_t idx = 0;
@@ -55,7 +57,7 @@ UTEST(Get, vector_homogenous)
 
 UTEST(Get, vector_hetereogenous)
 {
-    const auto vec = client("/array/heterogenous").get<std::vector<basic_type>>();
+    const auto vec = client("/array/heterogenous").get<compound_type>();
     ASSERT_EQ(vec.size(), 5);
     ASSERT_TRUE(std::get<bool>(vec.at(0)));
     ASSERT_EQ(std::get<int64_t>(vec.at(1)), 1);
@@ -79,8 +81,64 @@ UTEST(Set, basic_int)
     endpoint.set(orig_val);
     ASSERT_EQ(endpoint.get<int64_t>(), orig_val);
 }
-// TODO: more set tests
 
+UTEST(Set, basic_bool)
+{
+    auto endpoint = client("/basic/bool");
+    const auto orig_val = endpoint.get<bool>();
+    endpoint.set(true);
+    ASSERT_TRUE(endpoint.get<bool>());
+    endpoint.set(orig_val);
+    ASSERT_TRUE(endpoint.get<bool>() == orig_val);
+}
+
+UTEST(Set, basic_float)
+{
+    auto endpoint = client("/basic/float");
+    const auto orig_val = endpoint.get<float>();
+    endpoint.set(3.141F);
+    ASSERT_NEAR(endpoint.get<float>(), 3.141F, 1e-6);
+    endpoint.set(orig_val);
+    ASSERT_NEAR(endpoint.get<float>(), orig_val, 1e-6);
+}
+
+UTEST(Set, basic_string)
+{
+    auto endpoint = client("/basic/string");
+    const auto orig_val = endpoint.get<std::string>();
+    endpoint.set("Hello World!");
+    ASSERT_STREQ(endpoint.get<std::string>().c_str(), "Hello World!");
+    endpoint.set(orig_val);
+    ASSERT_STREQ(endpoint.get<std::string>().c_str(), orig_val.c_str());
+}
+
+UTEST(Set, vector_homogenous)
+{
+    auto endpoint = client("/array/homogenous");
+    const auto orig_vec = endpoint.get<compound_type>();
+
+    const std::vector<basic_type> set_vec{-1, 0, 1, 2, 3, 4, 5};
+    endpoint.set<compound_type>(set_vec);
+    const auto read_vec = endpoint.get<compound_type>();
+    ASSERT_TRUE(set_vec == read_vec);
+
+    endpoint.set<compound_type>(orig_vec);
+    ASSERT_TRUE(endpoint.get<compound_type>() == orig_vec);
+}
+
+UTEST(Set, vector_hetereogenous)
+{
+    auto endpoint = client("/array/heterogenous");
+    const auto orig_vec = endpoint.get<compound_type>();
+
+    const std::vector<basic_type> set_vec{false, "Hello", -100, 3.141F, 100, true};
+    endpoint.set<compound_type>(set_vec);
+    const auto read_vec = endpoint.get<compound_type>();
+    ASSERT_TRUE(set_vec == read_vec);
+
+    endpoint.set<compound_type>(orig_vec);
+    ASSERT_TRUE(endpoint.get<compound_type>() == orig_vec);
+}
 
 //
 // Test errors
@@ -119,6 +177,23 @@ UTEST(Errors, type_mismatch)
     ASSERT_TRUE(is_thrown);
 }
 
+UTEST(Errors, unlock)
+{
+    auto endpoint = client("/basic/int");
+    
+    bool is_thrown = false;
+    try
+    {
+        endpoint.unlock();
+    }
+    catch (const json_server::RuntimeException &e)
+    {
+        ASSERT_EQ(e.m_err_code, json_server::error_code::lock);
+        is_thrown = true;
+    }
+    ASSERT_TRUE(is_thrown);
+}
+
 //
 // Lock and unlock
 //
@@ -148,6 +223,16 @@ UTEST(Concurrency, lock)
     ASSERT_EQ(client_1.get<int64_t>(), 5050);
 }
 
+UTEST(Performance, read)
+{
+    auto endpoint = client("/basic/int");
+    const uint32_t ITERS = 10000;
+
+    for (uint32_t i = 0; i < ITERS; ++i)
+    {
+        volatile auto val = endpoint.get<int64_t>();
+    }
+}
 
 int main(int argc, char **argv)
 {
